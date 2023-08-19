f3d9c14a4e688aadca0b6ca5a8b43c032062ad02
MatheusRaul87 <matheusraul2011@hotmail.com>
2023-08-19 11:16:42 -0300

    arquivo de configuração

---
+++ app/config/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: "localhost",
+    USER:  "",
+    PASSWORD: "",
+    DB: "itemsdb",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 1000
+
+    }
+
+};
